473b2d039d43dcfaf929c7f409ccd217f8a268d2
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2023-06-16 23:49:33 -0300

    atualizando exercicio2 do curso

---
+++ EXERCÍCIOS/ex015/script.js
@@ -0,0 +1,45 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nas = document.querySelector("input#nascimento")
+    var res = document.querySelector("div#res")
+
+    if (nas.value.length == 0 || nas > (ano.value)){
+        window.alert ("[ERRO] Insera os valores novamente!")
+    }else {
+        var masculino = document.querySelector("input#masculino")
+        var feminino = document.querySelector("input#feminino")
+        var idade = (ano - Number(nas.value))
+        var genero = ""
+        var img = document.createElement("img")
+            img.setAttribute("id", "foto")
+
+        if (masculino.checked){
+            genero = "Homem"
+            if (idade < 14) {
+                img.setAttribute ("src", "bebe-m.png")
+            }else if (idade < 18) {
+                img.setAttribute ("src", "imagens/jovem-m.png")
+            }else if (idade < 50) {
+                img.setAttribute ("src", "imagens/adulto-m.png")
+            }else {
+                img.setAttribute ("src", "imagens/idoso-m.png")
+            }
+        } else if (feminino.checked){
+            genero = "Mulher"
+            if (idade < 14) {
+                img.setAttribute ("src", "imagens/bebe-m.png")
+            }else if (idade < 18) {
+                img.setAttribute ("src", "imagens/jovem-m.png")
+            }else if (idade < 50) {
+                img.setAttribute ("src", "imagens/adulto-m.png")
+            }else {
+                img.setAttribute ("src", "imagens/idoso-m.png")
+            }
+        }
+        res.style.textAlign = "center"
+        res.innerHTML = `Detectamos que você é ${genero} e tem ${idade} anos.`
+    }
+
+    
+}
